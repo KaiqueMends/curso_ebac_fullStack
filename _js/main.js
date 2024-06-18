cecbcf16ac3134f0a6b49c53719760c85c7b1b64
Kaique Mendes <kaique.mendesdev@gmail.com>
2024-06-17 22:31:56 -0300

    answer

--- _js/main.js
+++ _js/main.js
@@ -1,24 +1,37 @@
-const form = document.getElementById("form_deposito")
+const form = document.getElementById("myForm")
 
 function validaNumeros(numero1, numero2){
-    return numero1 > numero2;
+    return numero2 > numero1;
 }
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const numero1 = document.getElementById('primeiro_numero');
-    const numero2 = document.getElementById('segundo_numero');
-    const msgSucesso = `O número <b>${numero1.value}</b> é maior que <b>${numero2.value}</b>`;
+     // Obter os valores dos campos A e B
+    var campoA = parseInt(document.getElementById('primeiro_numero').value);
+    var campoB = parseInt(document.getElementById('segundo_numero').value);
+    
+     // Verificar se B é maior que A
+    if (campoB > campoA) {
+        showMessage(true);
+    } else {
+        showMessage(false);
+    } });
 
-    console.log(numero1.value)
-    console.log(numero2.value)
-    if (validaNumeros(numero1.value, numero2.value)){
-        const containerMsgSucesso = document.querySelector('.success_message');
-        containerMsgSucesso.innerHTML = msgSucesso;
-        containerMsgSucesso.style.display = 'block'
-        alert("tudo certo");
-    } else { alert("O número 1 deve ser maior que o número 2!")}
+function showMessage(valid) {
+    var messageElement = document.getElementById('message');
+    
+    if (valid) {
+        messageElement.textContent = 'Formulário válido! O número 2 é maior que o 1.';
+        messageElement.classList.add('valid');
+        messageElement.classList.remove('invalid');
+    } else {
+        messageElement.textContent = 'Formulário inválido! O número 2 precisa ser maior que 1.';
+        messageElement.classList.add('invalid');
+        messageElement.classList.remove('valid');
+    }
+    
+    messageElement.style.display = 'block';
 
-    console.log(validaNumeros(numero1.value, numero2.value))
-})
+    console.log(validaNumeros(numero2.value, numero1.value))
+}
