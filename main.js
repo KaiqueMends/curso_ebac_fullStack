2088a8ed8b86683ab6ab96fb7bd9b0e8c7abca0b
Kaique Mendes <kaique.mendesdev@gmail.com>
2025-02-03 16:43:47 -0300

    fist commit

--- main.js
+++ main.js
@@ -1,28 +1,18 @@
 $(document).ready(function(){
-    $('header button').click(function(){
-        $('form').slideDown();
-    })
-
-    $('#btnCancelar').click(function (e) { 
-        $('form').slideUp();
-        
-    });
-
-    $('form').on('submit', function (e) {
+    // Ao submeter o formulário, adiciona uma nova tarefa
+    $('#taskForm').on('submit', function(e) {
         e.preventDefault();
-        const enderecoImg = $('#urlAddImg').val();
-        const novaImg = $('<li style="display: none"></li>');
-        $(`<img src="${enderecoImg}" />`).appendTo(novaImg);
-        $(`<div class="overlay-img-li">
-                <a href="${enderecoImg}" target="_blank" title="Ver imagem em tamanho real">
-                    Ver imagem em tamanho real
-                </a>
-            </div>
-        `).appendTo(novaImg);
-        $(novaImg).appendTo('ul');
-        $(novaImg).fadeIn();
-        $('#urlAddImg').val('')
-    })
-
+        var tarefa = $('#taskInput').val().trim();
+        
+        if(tarefa !== "") {
+        // Cria um novo <li> com a tarefa
+        $('#taskList').append('<li>' + tarefa + '</li>');
+        $('#taskInput').val('');
+        }
+    });
     
-})
+    // Evento de clique para marcar/desmarcar a tarefa concluída
+    $('#taskList').on('click', 'li', function(){
+        $(this).toggleClass('completed');
+    });
+    });
